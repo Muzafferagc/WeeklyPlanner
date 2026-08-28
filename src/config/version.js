33789b53e1ed63cc70a7407862bea1f1628b56e8
Muzafferagc <muzafferagac011@gmail.com>
2026-08-28 03:28:02 +0300

    fix: Mobil gorunumde varsayilan plan modalinin ekrana sigmamasi ve kapatilamamasi hatasi CSS media query ile cozuldu v3.47.0

--- src/config/version.js
+++ src/config/version.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v3.46.0";
-export const LAST_UPDATED = "28 Ağustos 2026 - 03:22";
+export const APP_VERSION = "v3.47.0";
+export const LAST_UPDATED = "28 Ağustos 2026 - 03:27";
