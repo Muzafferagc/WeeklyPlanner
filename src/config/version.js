24ce3062e0e85d1a5187d4381c6d00384bea4d85
Muzafferagc <muzafferagac011@gmail.com>
2026-08-28 06:29:34 +0300

    fix: adjust mobile drawer width to prevent overflow

--- src/config/version.js
+++ src/config/version.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v3.8.0";
-export const LAST_UPDATED = "28 Ağustos 2026 - 06:24";
+export const APP_VERSION = "v3.8.1";
+export const LAST_UPDATED = "28 Ağustos 2026 - 06:29";
