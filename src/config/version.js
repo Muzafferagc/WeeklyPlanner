2711669492d2cbdc213aba6fe53376185d916577
Muzafferagc <muzafferagac011@gmail.com>
2026-08-30 20:19:03 +0300

    chore: bump version to v4.1.2

--- src/config/version.js
+++ src/config/version.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v4.1.1";
-export const LAST_UPDATED = "30 Ağustos 2026 - 20:15";
+export const APP_VERSION = "v4.1.2";
+export const LAST_UPDATED = "30 Ağustos 2026 - 20:18";
